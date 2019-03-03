f972276655d45b43947d004607c6a163b51be105
pione30 <g.pionelish30@gmail.com>
2019-03-03 22:57:04 +0900

    add previewIndicesSet to TetrominoIndices store

--- src/store/modules/TetrominoIndices.ts
+++ src/store/modules/TetrominoIndices.ts
@@ -11,6 +11,10 @@ class TetrominoIndices extends VuexModule {
     Array.from(Array(Tetrominos.length).keys())
   )
 
+  private previewIndicesSet: number[] = this.nextTetrominoIndicesSet
+    .slice(1)
+    .concat(this.nextNextTetrominoIndicesSet)
+
   private tetrominoIndicesIterator: IterableIterator<number> = this.nextTetrominoIndicesSet.values()
   private tetrominoIndicesResult: IteratorResult<number> = this.tetrominoIndicesIterator.next()
 
@@ -23,6 +27,16 @@ class TetrominoIndices extends VuexModule {
     // shallow copy
     this.nextTetrominoIndicesSet = this.nextNextTetrominoIndicesSet.slice()
     this.nextNextTetrominoIndicesSet = shuffle(this.nextNextTetrominoIndicesSet)
+  }
+
+  @Mutation
+  private SHIFT_PREVIEW_INDICES_SET(): void {
+    this.previewIndicesSet.shift()
+  }
+
+  @Mutation
+  private PUSH_APPLY_PREVIEW_INDICES_SET(): void {
+    this.previewIndicesSet.push.apply(this.previewIndicesSet, this.nextNextTetrominoIndicesSet)
   }
 
   @Mutation
@@ -57,10 +71,13 @@ class TetrominoIndices extends VuexModule {
     this.SET_NEXT_TETROMINO_INDICES_RESULT()
     if (this.tetrominoIndicesResult.done) {
       this.ROTATE_FORWARD_NEXT_TETROMINO_INDICES_SETS()
+      this.PUSH_APPLY_PREVIEW_INDICES_SET()
+
       this.SET_TETROMINO_INDICES_ITERATOR()
       this.SET_NEXT_TETROMINO_INDICES_RESULT()
     }
     this.SET_CURRENT_TETROMINO_INDEX()
+    this.SHIFT_PREVIEW_INDICES_SET()
   }
 
   @Action
@@ -75,6 +92,10 @@ class TetrominoIndices extends VuexModule {
     }
 
     this.SET_IS_HOLD_TETROMINO_USED(true)
+  }
+
+  public get getPreviewIndiceSet(): number[] {
+    return this.previewIndicesSet
   }
 
   public get getCurrentTetrominoIndex(): number {
